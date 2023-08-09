c2ca0ba83cf915c96b004a5f1fdf9d7b3c1df1f4
Vinicio Sarango <viniciosarango23@gmail.com>
2023-08-09 09:52:10 -0500

    primer paquete commit de mi proyecto

---
+++ gestionProductos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionproductosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestionProductos'
---
+++ gestionProductos/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(max_length=200, unique=True)
+    marca_producto = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    imagen = models.ImageField(upload_to='photos/products')
+
+    def __str__(self):
+        return self.nombre_producto
+
+class Cliente (models.Model):
+    nombre_cliente = models.CharField(max_length=50)
+    cedula_cliente = models.CharField(max_length=10)
+    direccion_cliente = models.CharField(max_length=250) 
+    telefono_cliente = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre_cliente
+
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha_venta = models.DateField("Fecha de venta", auto_now=True, auto_now_add=False)
+
+
